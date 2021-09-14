f0ea0fc786d857b9885fa4f3d942da94826d67ba
Edward Bramanti <edward@bramanti.org>
2021-09-14 11:38:10 -0600

    Add regression test for Coingecko token loading - Add mocha to env on .eslintrc - Update tests to demonstrate regression

--- tests/marketaggregator.ts
+++ tests/marketaggregator.ts
@@ -1,19 +1,28 @@
 import { expect } from "chai";
-import "mocha";
-
 import { MarketAggregator } from "../src/marketaggregator";
 
+const endpoint = "https://api.mainnet-beta.solana.com/";
+const STEP_ADDRESS = "StepAscQoEioFxxWGnh2sLBDFp9d8rvKz2Yp39iDpyT";
+
 describe("Market Aggregator", () => {
-  describe("Can", () => {
-    it("Aggregate", async () => {
-
-      const endpoint = "https://api.mainnet-beta.solana.com/";
-
+  describe("#queryLists", () => {
+    it("queries common currencies from token-list", async () => {
       const aggregator = new MarketAggregator(endpoint);
+
       await aggregator.queryLists();
+      expect(aggregator.tokenInfos.map(({ address }) => address)).to.include(
+        STEP_ADDRESS
+      );
+    });
+  });
+
+  describe("#querySources", () => {
+    it("queries common currencies and their prices", async () => {
+      const aggregator = new MarketAggregator(endpoint);
 
       const markets = await aggregator.querySources();
       expect(markets.length).to.gt(0);
+      expect(markets.map(({ address }) => address)).to.include(STEP_ADDRESS);
     });
   });
 });
