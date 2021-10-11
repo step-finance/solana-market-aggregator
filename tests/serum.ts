a6059e4908d04ce5fec85d0af063d30e7c8f49a2
Edward Bramanti <edward@bramanti.org>
2021-10-10 22:43:45 -0600

    Add market prices metadata to Serum source - Add optional metadata key to IMarketData - Update existing function to get all market prices (bid/ask/mid) - Add test to verify values being set on metadata

--- tests/serum.ts
+++ tests/serum.ts
@@ -1,84 +1,90 @@
-import { expect } from "chai";
 import "mocha";
+import { expect } from "chai";
 
+import { TokenInfo } from "@solana/spl-token-registry";
 import { SerumMarketSource } from "../src/sources/serum";
+import { ISerumMarketInfo } from "../src/types/serum";
+
+const testMarkets: ISerumMarketInfo[] = [
+  {
+    "address": "9wFFyRfZBsuAha4YcuxcXLKwMxJR43S7fPfQLusDBzvT",
+    "deprecated": false,
+    "name": "SOL/USDC",
+    "programId": "EUqojwWA2rd19FZrzeBncJsm38Jm1hEhE3zsmX3bRc2o"
+  },
+  {
+    "address": "A8YFbxQYFVqKZaoYJLLUVcQiWP7G2MeEgW5wsAQgMvFw",
+    "deprecated": false,
+    "name": "BTC/USDC",
+    "programId": "9xQeWvG816bUx9EPjHmaT23yvVM2ZWbrrpZb9PusVFin"
+  },
+];
+
+const testTokens: TokenInfo[] = [
+  {
+    "chainId": 101,
+    "address": "So11111111111111111111111111111111111111112",
+    "symbol": "SOL",
+    "name": "Wrapped SOL",
+    "decimals": 9,
+    "logoURI": "https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/So11111111111111111111111111111111111111112/logo.png",
+    "tags": [],
+    "extensions": {
+      "website": "https://solana.com/",
+      "serumV3Usdc": "9wFFyRfZBsuAha4YcuxcXLKwMxJR43S7fPfQLusDBzvT",
+      "serumV3Usdt": "HWHvQhFmJB3NUcu1aihKmrKegfVxBEHzwVX6yZCKEsi1",
+      "coingeckoId": "solana"
+    }
+  },
+  {
+    "chainId": 101,
+    "address": "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v",
+    "symbol": "USDC",
+    "name": "USD Coin",
+    "decimals": 6,
+    "logoURI": "https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v/logo.png",
+    "tags": [
+      "stablecoin"
+    ],
+    "extensions": {
+      "website": "https://www.centre.io/",
+      "coingeckoId": "usd-coin",
+      "serumV3Usdt": "77quYg4MGneUdjgXCunt9GgM1usmrxKY31twEy3WHwcS"
+    }
+  },
+  {
+    "chainId": 101,
+    "address": "9n4nbM75f5Ui33ZbPYXn59EwSgE8CGsHtAeTH5YFeJ9E",
+    "symbol": "BTC",
+    "name": "Wrapped Bitcoin (Sollet)",
+    "decimals": 6,
+    "logoURI": "https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/9n4nbM75f5Ui33ZbPYXn59EwSgE8CGsHtAeTH5YFeJ9E/logo.png",
+    "tags": [
+      "wrapped-sollet",
+      "ethereum"
+    ],
+    "extensions": {
+      "bridgeContract": "https://etherscan.io/address/0xeae57ce9cc1984f202e15e038b964bb8bdf7229a",
+      "serumV3Usdc": "A8YFbxQYFVqKZaoYJLLUVcQiWP7G2MeEgW5wsAQgMvFw",
+      "serumV3Usdt": "C1EuT9VokAKLiW7i2ASnZUvxDoKuKkCpDDeNxAptuNe4",
+      "coingeckoId": "bitcoin"
+    }
+  },
+];
+const endpoint = "https://api.mainnet-beta.solana.com/";
 
 describe("Serum Source", () => {
-  describe("Can", () => {
-    it("Request market prices", async () => {
+  it("requests market prices", async () => {
+    const serumMarketSource = new SerumMarketSource(testTokens, testMarkets, endpoint);
+    const prices = await serumMarketSource.query();
+    expect(prices.length).to.equal(2);
+  });
 
-      const testMarkets = [
-        {
-          "address": "9wFFyRfZBsuAha4YcuxcXLKwMxJR43S7fPfQLusDBzvT",
-          "deprecated": false,
-          "name": "SOL/USDC",
-          "programId": "EUqojwWA2rd19FZrzeBncJsm38Jm1hEhE3zsmX3bRc2o"
-        },
-        {
-          "address": "A8YFbxQYFVqKZaoYJLLUVcQiWP7G2MeEgW5wsAQgMvFw",
-          "deprecated": false,
-          "name": "BTC/USDC",
-          "programId": "9xQeWvG816bUx9EPjHmaT23yvVM2ZWbrrpZb9PusVFin"
-        },
-      ];
-
-      const testTokens = [
-        {
-          "chainId": 101,
-          "address": "So11111111111111111111111111111111111111112",
-          "symbol": "SOL",
-          "name": "Wrapped SOL",
-          "decimals": 9,
-          "logoURI": "https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/So11111111111111111111111111111111111111112/logo.png",
-          "tags": [],
-          "extensions": {
-            "website": "https://solana.com/",
-            "serumV3Usdc": "9wFFyRfZBsuAha4YcuxcXLKwMxJR43S7fPfQLusDBzvT",
-            "serumV3Usdt": "HWHvQhFmJB3NUcu1aihKmrKegfVxBEHzwVX6yZCKEsi1",
-            "coingeckoId": "solana"
-          }
-        },
-        {
-          "chainId": 101,
-          "address": "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v",
-          "symbol": "USDC",
-          "name": "USD Coin",
-          "decimals": 6,
-          "logoURI": "https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v/logo.png",
-          "tags": [
-            "stablecoin"
-          ],
-          "extensions": {
-            "website": "https://www.centre.io/",
-            "coingeckoId": "usd-coin",
-            "serumV3Usdt": "77quYg4MGneUdjgXCunt9GgM1usmrxKY31twEy3WHwcS"
-          }
-        },
-        {
-          "chainId": 101,
-          "address": "9n4nbM75f5Ui33ZbPYXn59EwSgE8CGsHtAeTH5YFeJ9E",
-          "symbol": "BTC",
-          "name": "Wrapped Bitcoin (Sollet)",
-          "decimals": 6,
-          "logoURI": "https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/9n4nbM75f5Ui33ZbPYXn59EwSgE8CGsHtAeTH5YFeJ9E/logo.png",
-          "tags": [
-            "wrapped-sollet",
-            "ethereum"
-          ],
-          "extensions": {
-            "bridgeContract": "https://etherscan.io/address/0xeae57ce9cc1984f202e15e038b964bb8bdf7229a",
-            "serumV3Usdc": "A8YFbxQYFVqKZaoYJLLUVcQiWP7G2MeEgW5wsAQgMvFw",
-            "serumV3Usdt": "C1EuT9VokAKLiW7i2ASnZUvxDoKuKkCpDDeNxAptuNe4",
-            "coingeckoId": "bitcoin"
-          }
-        },
-      ];
-
-      const endpoint = "https://api.mainnet-beta.solana.com/";
-
-      const serumSource = new SerumMarketSource(testTokens, testMarkets, endpoint);
-      const prices = await serumSource.query();
-      expect(prices.length).to.equal(2);
-    });
+  it("loads market prices data", async () => {
+    const serumMarketSource = new SerumMarketSource(testTokens, testMarkets, endpoint);
+    const serumSources = await serumMarketSource.query();
+    const serumSource = serumSources[0];
+    expect(serumSource.metadata).to.exist;
+    expect(serumSource.metadata!.marketPrices).to.have.all.keys("bid", "ask", "mid");
   });
 });
