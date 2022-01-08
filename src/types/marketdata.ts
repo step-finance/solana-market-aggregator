07a5eb6a05940020f82d1f24795602a35a8892ca
Edward Bramanti <edward@bramanti.org>
2022-01-07 17:05:02 -0800

    Flat export of utils folder Clean up duplicate RawMintInfo definition

--- src/types/marketdata.ts
+++ src/types/marketdata.ts
@@ -1,3 +1,5 @@
+import { RawMintInfo } from "../utils";
+
 export interface IMarketData {
   source: string;
   symbol: string;
@@ -8,14 +10,6 @@ export interface IMarketData {
 
 export type MarketDataMap = {
   [address: string]: IMarketData;
-};
-
-export type RawMintInfo = {
-  mintAuthority: string | null;
-  supply: string;
-  decimals: number;
-  isInitialized: boolean;
-  freezeAuthority: string | null;
 };
 
 export type MintInfoMap = {
