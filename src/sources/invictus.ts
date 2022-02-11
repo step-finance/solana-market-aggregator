b4f8979829b36b29054389639a2de991ab0150ca
Parker <parkerjallen1@gmail.com>
2022-02-11 11:29:25 -0800

    add locked staked invictus pricing (#33)
    * add locked staked invictus pricing
    
    * Add test
    
    Co-authored-by: Edward Bramanti <edward@bramanti.org>

--- src/sources/invictus.ts
+++ src/sources/invictus.ts
@@ -9,8 +9,12 @@ export const INVICTUS_MINT = "inL8PMVd6iiW3RCBJnr5AsrRN6nqr4BTrcNuQWQSkvY";
 export const STAKED_INVICTUS_MINT =
   "sinjBMHhAuvywW3o87uXHswuRXb3c7TfqgAdocedtDj";
 const STAKED_INVICTUS_VAULT = "5EZiwr4fE1rbxpzQUWQ6N9ppkEridNwbH3dU3xUf7wPZ";
+const LOCKED_STAKED_INVICTUS_VAULT =
+  "oybxAeqZ1zqricePm6skfNVtY9uHhACzazoKvcUXKXA";
+export const LOCKED_STAKED_INVICTUS_MINT =
+  "LsinpBtQH68hzHqrvWw4PYbH7wMoAobQAzcvxVHwTLv";
 /**
- * A class that retrieves market price of sIN
+ * A class that retrieves market price of sIN & lsIN
  */
 export class StakedInvictusMarketSource implements MarketSource {
   readonly connection: Connection;
@@ -27,7 +31,7 @@ export class StakedInvictusMarketSource implements MarketSource {
   /**
    * Queries the latest sIN price
    *
-   * @return Array containing one element which is sIN
+   * @return Object containing sIN and lsIN market data
    */
   async query(marketDataMap: MarketDataMap): Promise<MarketDataMap> {
     const invictusMarketData = marketDataMap[INVICTUS_MINT];
@@ -50,12 +54,34 @@ export class StakedInvictusMarketSource implements MarketSource {
 
     const stakedInvictusPrice = invictusMarketData.price * invictusRatio;
 
+    const lockedStakedVault = await cache.query(
+      this.connection,
+      LOCKED_STAKED_INVICTUS_VAULT,
+      TokenAccountParser
+    );
+    const lockedStakedInvictusMint = await cache.queryMint(
+      this.connection,
+      LOCKED_STAKED_INVICTUS_MINT
+    );
+    const totalInvictusLockedStaked = lockedStakedVault.info.amount;
+    const lockedInvictusRatio =
+      totalInvictusLockedStaked / lockedStakedInvictusMint.supply.toNumber();
+
+    const lockedStakedInvictusPrice =
+      invictusMarketData.price * lockedInvictusRatio;
+
     return {
       [STAKED_INVICTUS_MINT]: {
         source: "contract",
         symbol: "sIN",
         address: STAKED_INVICTUS_MINT,
         price: stakedInvictusPrice,
+      },
+      [LOCKED_STAKED_INVICTUS_MINT]: {
+        source: "contract",
+        symbol: "lsIN",
+        address: LOCKED_STAKED_INVICTUS_MINT,
+        price: lockedStakedInvictusPrice,
       },
     };
   }
