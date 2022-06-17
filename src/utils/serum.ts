bc22c0bcdb58fa9c829c6ed4c10f2046d1aba2ec
James Dunn <jamesallstar21@gmail.com>
2022-06-16 20:38:49 -0400

    seperated try and catches

--- src/utils/serum.ts
+++ src/utils/serum.ts
@@ -5,8 +5,14 @@ import type { ISerumMarketInfo } from "../types/serum";
 export const SERUM_MARKET_LIST_URL =
   "https://raw.githubusercontent.com/step-finance/serum-markets/main/src/markets.json" as const;
 
-export const getSerumMarketInfoMap = async (): Promise<ISerumMarketInfo[]> => {
-  const serumMarketResponse = await axios.get<ISerumMarketInfo[]>(SERUM_MARKET_LIST_URL);
-  const { data: serumMarketList } = serumMarketResponse;
-  return serumMarketList;
+export const getSerumMarketInfoMap = async (): Promise<ISerumMarketInfo[] | undefined> => {
+  try {
+    const serumMarketResponse = await axios.get<ISerumMarketInfo[]>(SERUM_MARKET_LIST_URL);
+    const { data: serumMarketList } = serumMarketResponse;
+    return serumMarketList;
+  } catch(err) {
+    console.log(err);
+    return;
+  }
+
 };
